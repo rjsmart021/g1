5fd7b56b941323e1672cd3b4bde2b476cfbd7384
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-09-18 19:01:16 -0500

    Update I.py

--- I.py
+++ I.py
@@ -1,45 +1,24 @@
- class Vehicle:
-        def __init__(self, reg_num, type, owner):
-            self.registration_number = reg_num
-            self.type = type
-            self.owner = owner
-
-        def update_owner(self, new_owner):
-            self.owner = new_owner
-
-       @classmethod
-        def isinstance_Vehehicle():
-            x = isinstance(Vehehicle, class)
-            print(x)
-
+class Vehicle:
+    def __init__(self, reg_num, type, owner):
+        self.registration_number = reg_num
+        self.type = type
+        self.owner = owner
+    def update_owner(self, new_owner):
+        self.owner = new_owner
 class Event:
     def __init__(self, name, date):
+        self.list_of_participants = []
         self.name = name
         self.date = date
-        self.keywords = keywords
-        self.add_particitants = list_of_participants.append()
-        self.get_participant_count = get_participant_count()
-    list_of_participants = []
- class Participant:  
-    def __init__(self, name, age):  
-        self.name = name  
-        self.age = age  
-    @classmethod
-    def Add_Participant(name, age):
-       return cls(name, age)  
-       return list_of_participants.append()
-       print(list_of_participants)
-    @classmethod
-    def get_participant_count(cls):
-        return f"Number of participants: 
-        {len(self.list_of_participants)}"
+    def add_participant(self, name, age):
+       self.list_of_participants.append((name,age))
+    def get_participant_count(self):
+        return f"Number of participants{len(self.list_of_participants)}"
 #Test 1
-v1 = Vehicle_1
-v1.reg_num = 44545
-v1.type = "Ferrari"
-v1.owner ="Gorla"
-print(Vehicle)
+v1 = Vehicle( 44545, "Ferrari", "Gorla")
+v1.update_owner("Margarette")
+print(v1.owner)
 #Test 2
 event1 = Event("Event test", "2024-10-10")
 event1.add_participant("daniel", 24)
-event1.get_participant_count()
+print(event1.get_participant_count())
